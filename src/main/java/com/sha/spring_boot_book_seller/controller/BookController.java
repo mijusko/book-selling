a71dc3c123db26249f61d22586fbc42b4de234c8
mijusko <mihajlo00kg@gmail.com>
2025-02-19 19:25:25 +0100

    Dodaj CORS

--- src/main/java/com/sha/spring_boot_book_seller/controller/BookController.java
+++ src/main/java/com/sha/spring_boot_book_seller/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/book")//pre-path
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController
 {
     @Autowired
