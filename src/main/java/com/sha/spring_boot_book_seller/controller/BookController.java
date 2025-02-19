9b98b514f1b299d0ffcaffae1e8840ea3474e096
mijusko <mihajlo00kg@gmail.com>
2025-02-19 19:35:55 +0100

    Ukloni CORS

--- src/main/java/com/sha/spring_boot_book_seller/controller/BookController.java
+++ src/main/java/com/sha/spring_boot_book_seller/controller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/book")//pre-path
-@CrossOrigin(origins = "http://localhost:4200")
 public class BookController
 {
     @Autowired
